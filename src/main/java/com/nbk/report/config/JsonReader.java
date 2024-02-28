6471a00758e8237788fe289e95890e9d5278f53e
Reda <ai.reda.mohsen@gmail.com>
2024-02-28 21:58:17 +0200

    Update JsonReader

--- src/main/java/com/nbk/report/config/JsonReader.java
+++ src/main/java/com/nbk/report/config/JsonReader.java
@@ -23,9 +23,9 @@ public class JsonReader {
                 Map.Entry<String, JsonNode> entry = nodesIterator.next();
                 String reportRoot = entry.getKey();
                 JsonNode reportNode = entry.getValue();
-                String dbConnection = reportNode.get("DB_Connection").asText();
-                String reportName = reportNode.get("ReportName").asText();
-                String sqlQuery = reportNode.get("SQL").asText();
+                String dbConnection = reportNode.get("DB_Connection").asText().strip();
+                String reportName = reportNode.get("ReportName").asText().strip();
+                String sqlQuery = reportNode.get("SQL").asText().strip();
                 List<Map.Entry<String, String>> reportFields = new ArrayList<>();
                 JsonNode reportFieldsNode = reportNode.get("ReportFields");
                 Iterator<Map.Entry<String, JsonNode>> fieldsIterator = reportFieldsNode.fields();
@@ -33,7 +33,7 @@ public class JsonReader {
                     Map.Entry<String, JsonNode> fieldEntry = fieldsIterator.next();
                     String fieldName = fieldEntry.getKey();
                     String fieldValue = fieldEntry.getValue().asText();
-                    reportFields.add(new AbstractMap.SimpleEntry<>(fieldName, fieldValue.strip()));
+                    reportFields.add(new AbstractMap.SimpleEntry<>(fieldName.strip(), fieldValue.strip()));
                 }
 
                 reportConfigurations.add(new ReportConfiguration(reportRoot, dbConnection, reportName, sqlQuery, reportFields));
