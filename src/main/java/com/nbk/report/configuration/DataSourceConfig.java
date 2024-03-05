0046fa82db97d3344244481c23fed053f9451e19
Reda <ai.reda.mohsen@gmail.com>
2024-03-06 00:51:38 +0200

    Add dynamic number of reports feature

---
+++ src/main/java/com/nbk/report/configuration/DataSourceConfig.java
@@ -0,0 +1,86 @@
+package com.nbk.report.configuration;
+
+import com.nbk.report.model.ReportConfigModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+@Configuration
+public class DataSourceConfig {
+    // Properties for database connection
+    private String url;
+    private String driverClassName;
+    private String username;
+    private String password;
+    @Bean
+    public DataSource createDataSource(ReportConfigModel reportConfiguration) {
+        // Get the database connection properties from ReportConfiguration
+        String dbConnectionProperties = reportConfiguration.getDbConnection();
+
+        // Check if the properties are not null and not empty
+        if (dbConnectionProperties != null && !dbConnectionProperties.isEmpty()) {
+            try {
+                // Split the properties into parts
+                String[] parts = dbConnectionProperties.split(";");
+
+                // Iterate through the parts and set the corresponding properties
+                for (String part : parts) {
+                    String[] keyValue = part.split("=");
+                    if (keyValue.length == 2) {
+                        String key = keyValue[0].trim();
+                        String value = keyValue[1].trim();
+
+                        // Use a switch statement to set the appropriate property based on the key
+                        switch (key) {
+                            case "url":
+                                setUrl(value);
+                                break;
+                            case "driver":
+                                setDriverClassName(value);
+                                break;
+                            case "username":
+                                setUsername(value);
+                                break;
+                            case "password":
+                                setPassword(value);
+                                break;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                // Print the stack trace if an exception occurs during property assignment
+                e.printStackTrace();
+            }
+        } else {
+            // Throw an exception if the database connection properties string is empty
+            throw new IllegalArgumentException("Database Connection properties string is empty!");
+        }
+
+        // Create a new DriverManagerDataSource and set its properties
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        // Return the configured DataSource
+        return dataSource;
+    }
+    // Setter methods for individual connection properties
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/nbk/report/configuration/ReportConfig.java
@@ -0,0 +1,31 @@
+package com.nbk.report.configuration;
+
+import com.nbk.report.model.ReportConfigModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ReportConfig {
+    private String defaultReportMap;
+
+    public ReportConfig(@Value("${map}") String defaultReportMap) {
+        this.defaultReportMap = defaultReportMap;
+    }
+    @Bean
+    public ReportConfigModel getCurrentReportConfiguration(@Value("${map}") String map){
+        List<ReportConfigModel> reportConfigurations = JsonReader.parseJson("conf.json");
+        for(ReportConfigModel reportConfiguration: reportConfigurations){
+            if (reportConfiguration.getReportConfigRoot().equals(map.trim())){
+                return reportConfiguration;
+            }
+        }
+        return null;
+    }
+    @Bean
+    public String getDefaultReportMap() {
+        return defaultReportMap;
+    }
+}
